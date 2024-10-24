1062256c3d26def138202bd09af4df8ea3226161
Tanishk Bansode <93660768+TanishkBansode@users.noreply.github.com>
2024-10-24 22:32:19 +0530

    Add files via upload

---
+++ code/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+    "path/filepath"
+
+)
+
+
+func main() {	 
+    dir, err := os.Getwd()
+	parentDir := filepath.Join(dir, "..", "blogs")
+
+	CheckNilError(err)
+
+    files, err := ioutil.ReadDir(parentDir)
+    CheckNilError(err)
+
+    fmt.Println("Files in directory:", parentDir)
+    for _, file := range files {
+        if !file.IsDir() { // Check if it's not a directory
+            fmt.Println(file.Name())
+        }
+    }
+}
+	
+
+
+func CheckNilError(err error){
+	if err != nil {
+		panic(err)
+	}
+}
