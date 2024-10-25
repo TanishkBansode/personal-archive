de625278108f30f94ad167487ce2263d402fed53
Tanishk Bansode <93660768+TanishkBansode@users.noreply.github.com>
2024-10-25 14:37:42 +0530

    Read files

--- code/main.go
+++ code/main.go
@@ -5,30 +5,51 @@ import (
     "io/ioutil"
     "os"
     "path/filepath"
-
+    "strings"
 )
 
 
 func main() {	 
-    // Step 1: Checks for all files in the blogs directory
+
+    // Step 1: Get the filenames and their content from the blogs directory
     dir, err := os.Getwd()
     parentDir := filepath.Join(dir, "..", "blogs")
-
     CheckNilError(err)
 
     files, err := ioutil.ReadDir(parentDir)
     CheckNilError(err)
 
-    fmt.Println("Files in directory:", parentDir)
+    fmt.Println(files)
+    var fileNames []string
     for _, file := range files {
-        if !file.IsDir() { // Check if it's not a directory
-            fmt.Println(file.Name())
+        if !file.IsDir() && strings.HasSuffix(file.Name(), ".txt") { // Check if it's not a directory
+	    fileNames = append(fileNames, file.Name())
         }
     }
+	
+    readit(fileNames)
 }
 	
+	
+// Reads a file, and currently prints in format "{Filename}: {content}"
+func readit(filenames []string) {
+    dir, err := os.Getwd()
+	
+    CheckNilError(err)
+
+    for _, filename := range filenames {
+        path := filepath.Join(dir, "..", "blogs", filename)
+	data, err := ioutil.ReadFile(path)
+	CheckNilError(err)
+	fmt.Printf("%s: %s\n", filename, string(data))
+		
+    }
+	
+	
+}
 
 
+// Checks err, it pretty handy
 func CheckNilError(err error){
 	if err != nil {
 		panic(err)
